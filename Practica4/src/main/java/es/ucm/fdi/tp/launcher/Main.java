d3eadc381f4ae541f69ee84ff714d1102e618ce7
Gerald Lima <gerky07@gmail.com>
2017-03-12 21:07:10 +0100

    Practica completa y funcional

--- Practica4/src/main/java/es/ucm/fdi/tp/launcher/Main.java
+++ Practica4/src/main/java/es/ucm/fdi/tp/launcher/Main.java
@@ -2,11 +2,13 @@ package es.ucm.fdi.tp.launcher;
 
 import es.ucm.fdi.tp.base.console.ConsolePlayer;
 import es.ucm.fdi.tp.base.model.GameAction;
+import es.ucm.fdi.tp.base.model.GameError;
 import es.ucm.fdi.tp.base.model.GamePlayer;
 import es.ucm.fdi.tp.base.model.GameState;
 import es.ucm.fdi.tp.base.player.RandomPlayer;
 import es.ucm.fdi.tp.base.player.SmartPlayer;
 import es.ucm.fdi.tp.ttt.TttState;
+import es.ucm.fdi.tp.was.WolfAndSheepState;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,8 +20,53 @@ import java.util.Scanner;
  */
 public class Main {
 
-	public static <S extends GameState<S, A>, A extends GameAction<S, A>> int playGame(GameState<S, A> initialState,
-			List<GamePlayer> players) {
+	
+	enum GameInfo{
+		
+		Ttt("ttt"), Was("was");
+		
+		private String id;
+		
+		GameInfo(String id) {
+			this.id = id;
+		}
+
+		public String getGameInfo() {
+			return id;
+		}
+
+		@Override
+		public String toString() {
+			return id;
+		}
+		
+		
+	}
+	
+	
+enum PlayerMode{
+		
+		CONSOLE("console"), RANDOM("rand"), INTELLIGENT("smart");
+		
+		private String id;
+		
+		PlayerMode(String id) {
+			this.id = id;
+		}
+
+		public String getPlayerMode() {
+			return id;
+		}
+
+		@Override
+		public String toString() {
+			return id;
+		}
+		
+		
+	}
+	public static <S extends GameState<S, A>, A extends GameAction<S, A>> 
+				int playGame(GameState<S, A> initialState, List<GamePlayer> players) {
 		int playerCount = 0;
 		for (GamePlayer p : players) {
 			p.join(playerCount++); // welcome each player, and assign
@@ -27,14 +74,15 @@ public class Main {
 		}
 		@SuppressWarnings("unchecked")
 		S currentState = (S) initialState;
-
+		System.out.println("Original State:\n" + currentState);
+		int cont= 1;
 		while (!currentState.isFinished()) {
 			// request move
 			A action = players.get(currentState.getTurn()).requestAction(currentState);
 			// apply move
 			currentState = action.applyTo(currentState);
-			System.out.println("After action:\n" + currentState);
-
+			System.out.println("After " + cont + " action:\n" + currentState);
+			cont++;
 			if (currentState.isFinished()) {
 				// game over
 				String endText = "The game ended: ";
@@ -50,67 +98,107 @@ public class Main {
 		return currentState.getWinner();
 	}
 
-	/*
-	public static void match(GameState<?, ?> initialState, GamePlayer a, GamePlayer b, int times) {
-		int va = 0, vb = 0;
-
-		List<GamePlayer> players = new ArrayList<GamePlayer>();
-		players.add(a);
-		players.add(b);
-
-		for (int i = 0; i < times; i++) {
-			switch (playGame(initialState, players)) {
-			case 0:
-				va++;
-				break;
-			case 1:
-				vb++;
-				break;
-			}
-		}
-		System.out.println("Result: " + va + " for " + a.getName() + " vs " + vb + " for " + b.getName());
-	}
-	*/
-
 	
+	
+	public static GameState<?,?> createInitialState(String gameName){
+		
+		if(gameName.equalsIgnoreCase(GameInfo.Ttt.getGameInfo())){
+			return new TttState(3);
+		}
+		
+		else if(gameName.equalsIgnoreCase(GameInfo.Was.getGameInfo())){
+			return new WolfAndSheepState(8);
+		}
+		
+		else
+			return null;	
+		
+	}
+
+	public static GamePlayer createPlayer(String gameName, String playerType, String playerName, Scanner s){
+		
+		
+		if(playerType.equalsIgnoreCase(PlayerMode.CONSOLE.getPlayerMode())){
+			  return new ConsolePlayer(playerName, s);
+		}
+		
+		else if(playerType.equalsIgnoreCase(PlayerMode.RANDOM.getPlayerMode())){
+			return new  RandomPlayer(playerName);
+		}
+		
+		else if(playerType.equalsIgnoreCase(PlayerMode.INTELLIGENT.getPlayerMode())){
+			return new  SmartPlayer(playerName, 5);
+		}
+		
+		else
+			
+			return null;
+	}
+	
+	
+	/**
+	 * Plays tick-tack-toe with a console player against a smart player. The
+	 * smart player should never lose.
+	 */
 	public static void testTtt() {
 		try (Scanner s = new Scanner(System.in)) {
 			List<GamePlayer> players = new ArrayList<GamePlayer>();
 			GameState<?, ?> game = new TttState(3);
-			players.add(new ConsolePlayer("Lobo", s));
-			players.add(new ConsolePlayer("Obeja", s));
+			players.add(new ConsolePlayer("Alice", s));
+			players.add(new SmartPlayer("AiBob", 5));
 			playGame(game, players);
+		} // <-- closes the scanner when the try-block ends
+	}
+
+	
+	public static boolean compruebaArgumentos(String [] palabras){
+		if(palabras.length == 3){
+			
+			if(!(palabras[0].equalsIgnoreCase("ttt") || palabras[0].equalsIgnoreCase("was")))
+				throw new GameError("Error: Nombre de juego incorrecto");
+		
+			if(!(palabras[1].equalsIgnoreCase("console") || palabras[1].equalsIgnoreCase("rand") || palabras[1].equalsIgnoreCase("smart")))
+				throw new GameError("Error: El tipo de jugador ´´" + palabras[1].toString() + "`` no está definido");
+		
+			else if(!(palabras[2].equalsIgnoreCase("console") || palabras[2].equalsIgnoreCase("rand") || palabras[2].equalsIgnoreCase("smart")))
+				throw new GameError("Error: El tipo de jugador ´´" + palabras[2].toString() + "`` no está definido");
+			return true;
+		}
+		else{
+			throw new GameError("Error: Número de argumentos inválido");
 		}
 	}
-	
-
-/******************************************************************/
-	
-	
-	public static GameState<?, ?> /*<S extends GameState<S, A>, A extends GameAction<S, A>>*/ createInitialState(String gameName){
-		return null;
-	}
-	
-	/*
-	 * gameName podrá ser cualquiera de las abreviaturas de juegos anteriorment
-	 * playerType podrá ser cualquiera de los tipos de jugador anteriormente descritos.
-	 */
-	
-	public static GamePlayer createPlayer(String gameName,String playerType, String playerName){
-		return null;
-		
-	}
-	public void createInitialState(){
-		
-	}
-	
 	/**
 	 * Main method.
 	 * 
 	 * @param args
 	 */
 	public static void main(String... args) {
-		 testTtt();
-		//match(new TttState(3), new SmartPlayer("AiAlice", 5), new RandomPlayer("AiBob"), 100);
+		// testTtt();
+		
+		try(Scanner s = new Scanner(System.in)){
+			
+		String [] palabras;
+		String  line;
+		
+		System.out.print("> ");
+		
+		line = s.nextLine();
+		line = line.trim();
+		palabras = line.split(" +");
+		
+		
+		if (compruebaArgumentos(palabras)){
+
+			List<GamePlayer> players = new ArrayList<GamePlayer>();
+			GameState<?, ?> game = createInitialState(palabras[0]);
+			players.add(createPlayer(palabras[0], palabras[1], "WOLF", s));
+			players.add(createPlayer(palabras[0], palabras[2], "SHEEPS", s));
+			playGame(game, players);
+		}
+	
+		}catch(GameError e){ //muestra todo el paquete no se porqueeeeeeeeeee
+			System.out.println(e);
+		}
 	}
 }
--- Practica4/src/main/java/es/ucm/fdi/tp/was/WolfAndSheepAction.java
+++ Practica4/src/main/java/es/ucm/fdi/tp/was/WolfAndSheepAction.java
@@ -2,21 +2,28 @@ package es.ucm.fdi.tp.was;
 
 import es.ucm.fdi.tp.base.model.GameAction;
 
-@SuppressWarnings("serial")
 public class WolfAndSheepAction implements GameAction<WolfAndSheepState, WolfAndSheepAction> {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private int player;
-	private int fil;
-	private int col;
+	private int filaOrigen;
+	private int colOrigen;
+	private int filaDestino;
+	private int colDestino;
 	
-	public WolfAndSheepAction(int jugador, int fila, int columna) {
-		player = jugador;
-		fil = fila;
-		col = columna;
+	public WolfAndSheepAction(int jugador, int fila, int columna, int filaDes, int colDes) {
+		
+		this.player = jugador;
+		this.filaOrigen = fila;
+		this.colOrigen = columna;
+		this.filaDestino = filaDes;
+		this.colDestino = colDes;
 	}
 	
-	
-	
+		
 	@Override
 	public int getPlayerNumber() {
 		return player;
@@ -25,7 +32,53 @@ public class WolfAndSheepAction implements GameAction<WolfAndSheepState, WolfAnd
 	@Override
 	public WolfAndSheepState applyTo(WolfAndSheepState state) {
 		
-		return null;
+		 if (player != state.getTurn()) {
+	            throw new IllegalArgumentException("Not the turn of this player");
+	        }
+		 
+		 int[][] board = state.getBoard();
+	        board[filaDestino][colDestino] = player;
+	        board[filaOrigen][colOrigen] = -1;
+	        
+	        // update state
+	        WolfAndSheepState next = new  WolfAndSheepState(state, board, false, -1);
+	        
+	        if (next.ganador(board, state.getTurn())) {
+	            next = new WolfAndSheepState(state, board, true, state.getTurn());
+	        }
+	        else if ((next.getTurn() == 0 && next.validActions(next.getTurn()).isEmpty()))
+	        	next = new WolfAndSheepState(state, board, true, state.getTurn());
+	        //TODO el lobo ya llega hasta el último movimiento
+	        else if (next.getTurn() == 1 && !state.ganador(board, state.getTurn()) && next.validActions(next.getTurn()).isEmpty())
+	        	next = new WolfAndSheepState(next, board, false, -1);
+	        else {
+	        	
+	            next = new WolfAndSheepState(state, board, false, -1);
+	        }
+	        return next;
+		
 	}
 
+	public int getFil() {
+		return filaDestino;
+	}
+
+	/*public void setFil(int fil) {
+		this.fil = fil;
+	}*/
+
+	public int getCol() {
+		return colDestino;
+	}
+
+	/*public void setCol(int col) {
+		this.col = col;
+	}*/
+	
+    public String toString() {
+    	
+        return "place " + player + " at (" + filaDestino + ", " + colDestino + ")";
+    }
+	
+	
 }
